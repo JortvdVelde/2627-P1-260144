c6fe587d904e0909d4d0325a4409515577e3d14e
JortvdVelde <jort@famvdvelde.nl>
2026-09-08 11:21:05 +0200

    Update

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -19,6 +19,7 @@ function draw() {
   line(0, 300, 600, 300);
   line(150, 75, 250, 75);
   line(0, 250, 300, 250);
+  line(0, 350, 300, 350);
 
 // Vertical lines
   line(100, 0, 100, 400);
@@ -27,12 +28,13 @@ function draw() {
   line(300, 0, 300, 400);
   line(400, 0, 400, 400);
   line(500, 0, 500, 400);
+  line(450, 100, 450, 400);
 
 // thickness of the lines
   strokeWeight(4);
   
 // Squares with different colors
-// fill(locatie, postitie, hoogte, hoogte)
+// 0 - 100 (Y-as)
   fill('yellow');
   rect(0, 0, 100, 50);
     
@@ -50,10 +52,56 @@ function draw() {
     
   fill('blue');
   rect(500, 0, 100, 100);
-  
+
+// 100 - 200 (Y-as)
   fill('red');
   rect(0, 100, 100, 100);
 
   fill('yellow');
   rect(150, 100, 100, 100);
+
+  fill('black')
+  rect(250, 100, 150, 100);
+
+  fill('blue')
+  rect(450, 100, 50, 100);
+
+  ('black');
+  rect(100, 200, 50, 50);
+
+// 200 - 300 (Y-as)
+  fill('blue');
+  rect(0, 250, 100, 50);
+
+  fill('red');
+  rect(150, 250, 100, 50);
+
+  fill('yellow')
+  rect(300, 200, 100, 100);
+
+  fill('black')
+  rect(450, 200, 50, 100);
+
+// 300 - 400 (Y-as)
+  fill('yellow')
+  rect(0, 300, 100, 50);
+
+  fill('black')
+  rect(100, 350, 50, 50);
+
+  fill('blue');
+  rect(150, 300, 100, 50);
+
+  fill('black');
+  rect(250, 300, 50, 50);
+
+  fill('red');
+  rect(300, 300, 100, 100);
+
+  fill('blue');
+  rect(400, 300, 50, 100);
+
+  fill('yellow');
+  rect(500, 300, 100, 100);
+
 }
--- Week 1 - Hello World/sketch.js
+++ Week 1 - Hello World/sketch.js
@@ -1,7 +1,28 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 800);
 }
 
 function draw() {
   background(220);
+
+  fill('black')
+  // 1. Name
+  textSize(20);
+  text('1. Jort van der Velde', 50, 50);
+
+  // 2. Flag
+  textSize(20);
+  text('2.', 50, 100);
+
+  fill('red');
+  rect(80, 75, 200, 50);
+  fill('white');
+  rect(80, 125, 200, 50);
+  fill('blue');
+  rect(80, 175, 200, 50);
+  fill('black');
+
+  // 3. Chessboard
+  textSize(20);
+  text('3.', 50, 260);
 }
